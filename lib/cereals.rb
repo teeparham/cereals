e2f5f1b801ecd0c4a27c8c16447f7218f34ebdea
Tee Parham <tee@neighborland.com>
2017-05-26 16:55:09 -0600

    Add rubocop

--- lib/cereals.rb
+++ lib/cereals.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require "cereals/version"
 require "cereals/array_serializer"
 require "cereals/base"
--- lib/cereals/version.rb
+++ lib/cereals/version.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Cereals
   VERSION = "0.0.0"
 end
